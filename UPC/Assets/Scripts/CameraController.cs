0e038509e1752d1c7199ba96ac1a0ce4ecd8ad44
Bogdan713 <bogdanprostov@gmail.com>
2019-01-14 07:17:35 +0200

    data replaced

---
+++ UPC/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Transform target;
+    private float speed = 2.0f;
+
+    private void Awake()
+    {
+        if (!target) target = FindObjectOfType<Character>().transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 position = target.position;
+        position.z = -10.1f;
+        transform.position = Vector3.Lerp(transform.position, position, speed*Time.deltaTime);
+	}
+}
---
+++ UPC/Assets/Scripts/Character.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterState { Idle, Move}
+
+public class Character : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public float speed = 4f;
+    //public float jumpForce = 1f;
+    public int lives;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        lives = 5;
+    }
+
+    private CharacterState State
+    {
+        get { return (CharacterState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    void Move()
+    {
+        Vector3 vectorH = Vector3.right * Input.GetAxis("Horizontal");
+        Vector3 vectorV = Vector3.up * Input.GetAxis("Vertical");
+        print(Input.GetAxis("Horizontal"));
+        print(Input.GetAxis("Vertical"));
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + vectorH, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + vectorV, speed * Time.deltaTime);
+        State = CharacterState.Move;
+    }
+
+    void Attack()
+    {
+        //State = CharacterState.AttackDown;
+    }
+
+    bool canMove(int dir) {
+        Vector3 position = transform.position;
+        //change on grow
+
+        //if(  (new Vector3(position.x+x, position.y+ y)) )
+        transform.GetComponent<Collider2D>();
+        return true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        State = CharacterState.Idle;
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            Move();
+        }
+        if (Input.GetKey("space"))
+        {
+            Attack();
+        }
+    }
+}
